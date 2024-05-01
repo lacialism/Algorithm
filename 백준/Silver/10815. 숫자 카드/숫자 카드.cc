fb0165345ca00b4d3785ba907a94b3d52daa1a75
lacialism <128123296+lacialism@users.noreply.github.com>
2024-05-02 01:28:01 +0900

    [Silver V] Title: 숫자 카드, Time: 196 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <vector>
-
-void mergeSort(vector<int>& c, int low, int high);
-void merge(vector<int>& c, int low, int high);
+#include <algorithm>
 
 int main()
 {
@@ -18,7 +16,7 @@ int main()
 	c = vector<int>(n);
 	for (int i = 0; i < n; i++)
 		cin >> c[i];
-	mergeSort(c, 0, n - 1);
+	sort(c.begin(), c.end());
 
 	cin >> m;
 	for (int i = 0; i < m; i++) {
@@ -43,43 +41,4 @@ int main()
 	}
 
 	return 0;
-}
-
-void mergeSort(vector<int>& c, int low, int high)
-{
-	if (low + 1 < high) {
-		int mid = (low + high) / 2;
-		mergeSort(c, low, mid);
-		mergeSort(c, mid + 1, high);
-		merge(c, low, high);
-	}
-	else if (low + 1 == high) {
-		if (c[low] > c[high]) {
-			int temp = c[low];
-			c[low] = c[high];
-			c[high] = temp;
-	}	}
-}
-
-void merge(vector<int>& c, int low, int high)
-{
-	vector<int> w(high - low + 1);
-	int mid = (low + high) / 2;
-	int p1 = low, p2 = mid+1;
-
-	int p = 0;
-	while (p1 <= mid && p2 <= high) {
-		if (c[p1] < c[p2])
-			w[p++] = c[p1++];
-		else
-			w[p++] = c[p2++];
-	}
-
-	while (p1 <= mid)
-		w[p++] = c[p1++];
-	while (p2 <= high)
-		w[p++] = c[p2++];
-
-	for (p = low; p <= high; p++)
-		c[p] = w[p-low];
 }
